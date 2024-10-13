fa6b964c3317a1e7d697d8a76e87c527d5862396
Luka <lukajeremic556@gmail.com>
2024-10-13 18:29:04 +0200

    First Commit

---
+++ Habits/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = @"
+    CREATE TABLE IF NOT EXISTS drinking_water (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Date TEXT,
+    Quantity INTEGER
+    )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
+
+
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
